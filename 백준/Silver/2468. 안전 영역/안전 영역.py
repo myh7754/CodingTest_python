6c3f878d648c46bb3fb7d3f967514eb8de750a38
myh7754 <myh4755@gmail.com>
2025-07-31 20:43:00 +0900

    [Silver I] Title: 안전 영역, Time: 672 ms, Memory: 34992 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+n = int(input())
+graph = [list(map(int, input().split())) for i in range(n)]
+
+
+def create(c_graph, target):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] <= target:
+                c_graph[i][j] = 1
+
+def bfs(y,x, graph):
+    myque = deque()
+    myque.append((y,x))
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    graph[y][x] = 1
+    count = 1
+    while myque:
+        y, x = myque.popleft()
+        for i in range(4):
+            nx = dx[i] +x
+            ny = dy[i] +y
+            if 0<=nx<n and 0<=ny<n:
+                if graph[ny][nx] == 0:
+                    graph[ny][nx] = 1
+                    myque.append((ny,nx))
+                    count +=1
+    return count
+m = 0
+for i in range(0,101):
+    c_graph = [[0]*n for _ in range(n)]
+    result = []
+    create(c_graph,i)
+    for j in range(n):
+        for k in range(n):
+            if c_graph[j][k] == 0:
+                result.append(bfs(j,k,c_graph))
+    l = len(result)
+    m = max(m,l)
+    
+
+print(m)
