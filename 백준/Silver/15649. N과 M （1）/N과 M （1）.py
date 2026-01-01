b13d6b59a75f3ec0c9089aaf7bfe9b67f5417273
myh7754 <myh4755@gmail.com>
2026-01-01 23:50:02 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+import sys
+input =lambda : sys.stdin.readline().rstrip()
+from collections import deque
+n , m= map(int, input().split())
+isused = [False] * (n+1)
+arr = [0]*m
+def func(k):
+
+    if k == m:
+        print(*arr)
+        return
+    for i in range(1, n+1):
+        if not isused[i]:
+            isused[i] = True
+            arr[k] = i
+            func(k+1)
+            isused[i]= False
+func(0)
