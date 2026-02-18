d8ab1ddce2535bb6d9801c764d983d50da0cfa44
myh7754 <myh4755@gmail.com>
2026-02-18 16:37:41 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2196 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -41,7 +41,7 @@ int main() {
     // 외부 공기와 닿아 있는 치즈 부분을 저장하고 O
     // 해당 저장을 0으로 변경 O
     // 만약 남은 그래프의 치즈가 0이라면 마지막 저장한 치즈 부분의 사이즈가 정답
-    int day = 0;
+    int day ;
     int cnt;
     while(true) {
         if (!cntCz()) {
