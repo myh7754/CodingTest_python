15803322652681cb9f0ac09026c696df8eb1ddbe
myh7754 <myh4755@gmail.com>
2026-02-18 16:36:46 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2196 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -1,76 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<vector<int>> v, visited;
-int gy, gx,ret,count;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
+int gy,gx;
+vector<vector<int>> v,visited;
+
+int dy[] = {0,0,-1,1};
+int dx[] = {-1,1,0,0};
+
 void dfs(int y, int x) {
-    for (int i=0; i<4; i++) {
+    for (int i =0; i<4; i++) {
+        int nx = dx[i] +x;
         int ny = dy[i] +y;
-        int nx = dx[i] + x;
-        if(0<=ny && ny<gy && 0<=nx && nx<gx) {
-            if (visited[ny][nx]== 1) continue;
-            if (v[ny][nx] == 0) {
-                visited[ny][nx] =1;
-                dfs(ny,nx);   
-            }
+        if (0<=ny && ny <gy && 0<=nx && nx < gx) {
+            if (visited[ny][nx] == 1 || v[ny][nx] == 1) continue;
+            visited[ny][nx] =1;
+            dfs(ny,nx);
         }
     }
 }
 
-int countCheese() {
-    int cnt = 0;
-    for (int y=0; y<gy; y++)
-        for (int x=0; x<gx; x++)
-            if (v[y][x] == 1) cnt++;
+int cntCz() {
+    int cnt =0;
+    for (int i=0; i<gy; i++) {
+        for (int j=0; j<gx; j++) {
+            if (v[i][j] == 1) {
+                cnt++;
+            }
+        }
+    }
     return cnt;
 }
-
-
 int main() {
-    cin >> gy >> gx;
-    v.assign(gy, vector<int>(gx));
+    cin >> gy >>gx;
+    v.assign(gy,vector<int>(gx));
     for (int i=0; i<gy; i++) {
-        for (int j =0; j<gx; j++) {
+        for (int j=0; j<gx; j++) {
             cin >> v[i][j];
         }
     }
-    
-    int time = 0;
-    int last = 0;
-    while (true) {
-        if (countCheese() ==0) {
+    // 외부 공기를 전부 방문처리, 날짜 카운트 시작 O
+    // 외부 공기와 닿아 있는 치즈 부분을 저장하고 O
+    // 해당 저장을 0으로 변경 O
+    // 만약 남은 그래프의 치즈가 0이라면 마지막 저장한 치즈 부분의 사이즈가 정답
+    int day = 0;
+    int cnt;
+    while(true) {
+        if (!cntCz()) {
             break;
         }
-        visited.assign(gy, vector<int>(gx, 0));
-        visited[0][0] = 1;
-        dfs(0, 0);
-        vector<pair<int,int>> melt;
-        for (int y=0; y<gy; y++) {
-            for (int x =0; x<gx; x++) {
-                if (v[y][x] !=1) continue; // 치즈 부분만 고르기
-                for(int dir=0; dir<4; dir++) {
-                    int ny = y + dy[dir];
-                    int nx = x + dx[dir];
-                    if (ny<0||ny>=gy||nx<0||nx>=gx) continue;
-                    // 치즈 중에 공기와 겉에 공기에 맡닿아 있는 부분만 고르기
-                    // 반드시 외부 공기 0과 닿아있는 부분만 추가 되므로 내부의 0은 신경쓸 필요 없음
-                    if (visited[ny][nx] == 1) {
-                        melt.push_back({y,x});
-                        break;
+        visited.assign(gy,vector<int>(gx));
+        dfs(0,0);
+        day++;
+        vector<pair<int,int>> cz;
+        for (int y =0; y<gy; y++ ) {
+            for (int x=0; x<gx; x++) {
+                for (int i =0; i<4; i++) {
+                    int nx = dx[i] +x;
+                    int ny = dy[i] +y;
+                    if (0<=ny && ny <gy && 0<=nx && nx < gx) {
+                        if(visited[ny][nx]==1 && v[y][x] == 1) {
+                            cz.push_back({y,x});
+                            break;
+                        }
                     }
-                }
+                }                
             }
         }
-        // 녹이기
-        int count =0;
-        for (auto [y,x] : melt) {
-            v[y][x] = 0;
-            count++;
+        cnt = cz.size();
+        for (auto it : cz) {
+            v[it.first][it.second] = 0;
         }
-        time++;
-        last = count; 
     }
 
-    cout << time << "\n" << last << "\n";
+    cout << day << "\n" << cnt;
+    return 0;
 }
