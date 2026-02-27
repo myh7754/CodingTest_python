2dd6ff5ac13efbe2a080b5ba098253ffb7697bf5
myh7754 <myh4755@gmail.com>
2026-02-28 03:04:28 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> cnt = new HashMap<>();
-
-        // 1) 종류별 개수 세기
+        int i = 1;
         for (String[] c : clothes) {
-            String type = c[1];
-            cnt.put(type, cnt.getOrDefault(type, 0) + 1);
+            cnt.put(c[1], cnt.getOrDefault(c[1],0) +1);
+            
         }
-
-        // 2) (개수 + 1) 전부 곱하기
-        int ans = 1;
-        for (int n : cnt.values()) {
-            ans *= (n + 1);
+        for (int k : cnt.values()) {
+            i = i*(1+k);
         }
-
-        // 3) 전부 안 입는 경우 1개 빼기
-        return ans - 1;
+        return i-1;
     }
 }
